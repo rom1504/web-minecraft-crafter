7c2709a241041da85e7659721aca522303e28e2d
Romain Beaumont <romain.rom1@gmail.com>
2016-03-19 02:55:05 +0100

    add some images

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const findItemOrBlockByName=require("minecraft-data")("1.8").findItemOrBlockByName;
 const niceCraft=require("minecraft-crafter").niceCraft;
 const craft=require("minecraft-crafter").craft;
+const assets=require("minecraft-assets")("1.8.8").textureContent;
+const textureContentArray=require("minecraft-assets")("1.8.8").textureContentArray;
 const $=require("jquery");
 
 global.jQuery = $;
@@ -8,15 +10,40 @@ global.jQuery = $;
 require("jquery-autocomplete");
 require('jquery-ui');
 
-const itemNames=require("minecraft-data")("1.8").blocksArray
-  .concat(require("minecraft-data")("1.8").itemsArray)
-  .map(item => item.name);
+const item=$("#item");
 
-$("#item").autocomplete({
-  source:itemNames
+item.autocomplete({
+  source:textureContentArray.map(o => ({
+    value:o.name,
+    label:o.name,
+    icon:o.texture
+  })),
+  focus: function( event, ui ) {
+    item.val( ui.item.value );
+    return false;
+  }
 });
+
+item.data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+  var $li = $('<li>'),
+    $img = $('<img>');
+  $img.attr({
+    src: item.icon,
+    alt: item.label
+  });
+
+  $li.attr('data-value', item.label);
+  $li.append('<a href="#">');
+  $li.find('a').append($img).append(item.label);
+
+  return $li.appendTo(ul);
+};
 
 $("#form").submit(e => {
   e.preventDefault();
-  $("#result").text(niceCraft(craft({id:findItemOrBlockByName($("#item").val()).id,count:1})));
+  const name=$("#item").val();
+
+  $("#result").text(niceCraft(craft({id:findItemOrBlockByName(name).id,count:1})));
+
+  $("#craftResult").html("<img width='100px' src='"+assets[name].texture+"' />");
 });
